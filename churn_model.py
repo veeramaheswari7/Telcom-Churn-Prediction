f318bd0d32b94cb56ae53a70793993a1259f0254
veeramaheswari7 <veeramaheswarijayampu123@gmail.com>
2026-04-27 12:58:41 +0530

    Initial churn prediction pipeline added

---
+++ churn_model.py
@@ -0,0 +1,26 @@
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.metrics import accuracy_score
+
+df = pd.read_csv("data/Telecom Customer Churn.csv")
+
+df = df.dropna()
+
+X = df.drop("Churn", axis=1)
+y = df["Churn"]
+
+X = pd.get_dummies(X)
+
+X_train, X_test, y_train, y_test = train_test_split(
+    X, y, test_size=0.2, random_state=42
+)
+
+model = RandomForestClassifier()
+model.fit(X_train, y_train)
+
+pred = model.predict(X_test)
+
+acc = accuracy_score(y_test, pred)
+
+print("Accuracy:", acc)
